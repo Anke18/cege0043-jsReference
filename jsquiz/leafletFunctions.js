ee6c21ee6a6838cc3a7cee6e7ca5855d5a44a529
Anke18 <uczlaw3@ucl.ac.uk>
2019-03-21 21:27:31 -0400

    AF1, modify get correct num

--- jsquiz/leafletFunctions.js
+++ jsquiz/leafletFunctions.js
@@ -4,22 +4,11 @@ Code for Version4 for AF1
 // create a variable that will hold the XMLHttpRequest()
 var client;
 var xhrQuestionData;
+var xhrAnswerData;
 var questionLayer;
 var questionJSON;
 
-/* Delete this function, only one popup once, in order to not close the question popup
-// for questions, get location
-function onMapClick(e)
-{
-	// create a custom popup
-	var popupLocation = L.popup();
-	// location: e.latlng.lat, e.latlng.lng
-	// e.latlng -> eg. LatLng(51.50009, -0.08748)
-	popupLocation.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(mymap);
-}
-// now add the click event detector to the map
-mymap.on('click', onMapClick);
-*/
+
 // here modify code for core functionality1
 function startQuestionDataLoad()
 {
@@ -49,12 +38,38 @@ function questionDataResponse()
 	}
 }
 
+//-------------------------
+function startAnswerDataLoad()
+{
+	xhrAnswerData = new XMLHttpRequest();
+	var url = "http://developer.cege.ucl.ac.uk:"+ httpPortNumber;
+	url = url + "/getCorrectNum/"+ httpPortNumber;
+	xhrAnswerData.open("GET", url, true);
+	xhrAnswerData.onreadystatechange = answerDataResponse;
+	xhrAnswerData.send();
+}
+
+function answerDataResponse()
+{
+	if(xhrAnswerData.readyState == 4)
+	{
+		// if data is ready, process the data
+		var answerData = xhrAnswerData.responseText;
+		var countStr = answerData.substring(36);//[{"array_to_json":[{"num_questions":2}]}] delete frount
+		var countNum = parseInt(countStr);//get number
+		document.getElementById("answerCorrectResult").innerHTML = "You have correctly answered " + countNum +" questions!";
+	}
+}
+
+//----------------------
+
 // could only test on the phone, which makes it hard to debug
 // have to apart the function to let the questionJSON to be used to get the closestQuiz, and send it back...(mad
 function loadQuestionData1(questionData)
 {
 	questionJSON = JSON.parse(questionData);
 	navigator.geolocation.getCurrentPosition(getDistanceFromMultiplePoints);
+	loadQuestionData2();//load in web?
 }
 
 function loadQuestionData2()
@@ -82,12 +97,13 @@ function loadQuestionData2()
 			htmlString = htmlString + "<input type='radio' name='useranswer' id='"
 			+feature.properties.id+"_4'/>"+feature.properties.answer_4+"<br><br>";
 			
-			htmlString = htmlString + "<button onclick='checkAnswer("
-			+feature.properties.id + ");return false;'>Submit Answer</button>";
+			htmlString = htmlString + "<button onclick='checkAnswer("+feature.properties.id+ 
+			");return false;'>Submit Answer</button>";
 			
 			htmlString = htmlString + "<div id=answer" + feature.properties.id + " hidden>"+feature.properties.correct_answer+"</div>";
 			// add div to show upload answer results
 			htmlString = htmlString + "<br/><div id=answerUploadResult> The result of the upload goes here </div>";
+			htmlString = htmlString + "<br/><div id=answerCorrectResult> The count of the correct number goes here </div>";
 			htmlString = htmlString + "</div>";
 			//return L.marker(latlng).bindPopup(htmlString);
 			//alert(closestQuiz+"kkk :" +feature.properties.id);
@@ -106,9 +122,11 @@ function loadQuestionData2()
 		},
 	}).addTo(mymap);
 	openMarker.openPopup();
-	mymap.setView(openSet, 16);
+	mymap.setView(openSet, 18);
 	//mymap.fitBounds(questionLayer.getBounds());
+	//mymap.setView(openSet, 16);
 }
+
 
 function checkAnswer(questionID) 
 {
@@ -136,6 +154,27 @@ function checkAnswer(questionID)
 	}
 	// create answer string to send back
 	var postAnswerString ="port_id="+httpPortNumber+"&question_id="+questionID+"&answer_selected="+answerSelected+"&correct_answer="+answer;
+	
+	/*change color test
+	questionLayer.eachLayer(function(layer)
+	{
+		//alert(layer.feature.geometry.coordinates);
+		if(layer.feature.properties.id == questionID && correctAnswer === true)
+		{
+			layer.closePopup();
+			var MarkerRed = L.AwesomeMarkers.icon({markerColor: 'orange'});
+			L.marker([layer.feature.geometry.coordinates[1],layer.feature.geometry.coordinates[0]], {icon:MarkerRed}).addTo(mymap);
+		}
+		if(layer.feature.properties.id == questionID && correctAnswer === false)
+		{
+			layer.closePopup();
+			var MarkerPink = L.AwesomeMarkers.icon({markerColor: 'pink'});
+			L.marker([layer.feature.geometry.coordinates[1],layer.feature.geometry.coordinates[0]], {icon:MarkerPink}).addTo(mymap);
+		}
+	});*/
+	
 	// uploadData.js for sending back answers
 	startAnswerUpload(postAnswerString);
+	// count number
+	startAnswerDataLoad();
 }
